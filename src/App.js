3f5f3cc4153ef21bc17a5e9c6b485e4ac77fe883
ramilabbaszade <abbasovramil65@gmail.com>
2022-03-16 00:15:02 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [gameGrid, setGameGrid] = useState([
+    "","","",
+    "","","",
+    "","","",
+  ]);
+  // const winningMatchs = [1,2,3]
+  const [currentPlayer,setCurrentPlayer] = useState(1)
+  const [firstPlayerTicks,setFirstPlayerTicks] = useState([])
+  const [secondPlayerTicks,setSecondPlayerTicks] = useState([])
+
+  const setGameCell = async (index) => {
+    let inputValue = currentPlayer === 1 ? "X" : "O"
+    setGameGrid(
+      gameGrid.map((btn, i) => {
+          if (!btn && index === i) return inputValue;
+          else return btn;
+      })
+    );
+    setCurrentPlayer(currentPlayer === 1 ? 2 : 1)
+    if(currentPlayer === 1){
+      firstPlayerTicks.push(index)
+    }else{
+      secondPlayerTicks.push(index)
+    }
+
+    setFirstPlayerTicks(firstPlayerTicks)
+    setSecondPlayerTicks(secondPlayerTicks)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tic Tac Toe</h1>
+      <div className="game_canvas">
+        {currentPlayer}
+        <div className="container">
+          {gameGrid.map((cell, index) => {
+            return (
+              <div
+                key={index}
+                className="cell"
+                onClick={() => setGameCell(index)}
+              >
+                <span>{cell}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
