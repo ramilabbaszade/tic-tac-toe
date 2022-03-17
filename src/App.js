d6321b44ea999db467803f6d7398a494a5bb9834
ramilabbaszade <abbasovramil65@gmail.com>
2022-03-17 19:39:52 +0400

    feat: basic game logic

--- src/App.js
+++ src/App.js
@@ -3,32 +3,53 @@ import "./App.css";
 
 function App() {
   const [gameGrid, setGameGrid] = useState([
-    "","","",
-    "","","",
-    "","","",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
   ]);
-  // const winningMatchs = [1,2,3]
-  const [currentPlayer,setCurrentPlayer] = useState(1)
-  const [firstPlayerTicks,setFirstPlayerTicks] = useState([])
-  const [secondPlayerTicks,setSecondPlayerTicks] = useState([])
+  const winningMatchs = [1,2,3]
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [firstPlayerTicks, setFirstPlayerTicks] = useState([]);
+  const [secondPlayerTicks, setSecondPlayerTicks] = useState([]);
 
   const setGameCell = async (index) => {
-    let inputValue = currentPlayer === 1 ? "X" : "O"
+    if(gameGrid[index] === "")
+    {let inputValue = currentPlayer === 1 ? "X" : "O";
     setGameGrid(
       gameGrid.map((btn, i) => {
-          if (!btn && index === i) return inputValue;
-          else return btn;
+        if (!btn && index === i) return inputValue;
+        else return btn;
       })
     );
-    setCurrentPlayer(currentPlayer === 1 ? 2 : 1)
-    if(currentPlayer === 1){
-      firstPlayerTicks.push(index)
-    }else{
-      secondPlayerTicks.push(index)
+    if (currentPlayer === 1 && firstPlayerTicks.length < 3) {
+      firstPlayerTicks.push(index);
+    } else if(currentPlayer === 2 && secondPlayerTicks.length < 3) {
+      secondPlayerTicks.push(index);
     }
 
-    setFirstPlayerTicks(firstPlayerTicks)
-    setSecondPlayerTicks(secondPlayerTicks)
+    setFirstPlayerTicks(firstPlayerTicks);
+    setSecondPlayerTicks(secondPlayerTicks);
+    setCurrentPlayer(currentPlayer === 1 ? 2 : 1);
+
+    console.warn("firstPlayerTicks",firstPlayerTicks.toString());
+    console.warn("secondPlayerTicks",secondPlayerTicks.toString());}
+    if(is_same(firstPlayerTicks,winningMatchs)){
+      alert("First player WON")
+    }
+    if(is_same(secondPlayerTicks,winningMatchs)){
+      alert("Second player WON")
+    }
+    
+    
+  };
+  const is_same = (array1, array2) => {
+    return array1.toString() === array2.toString()
   };
 
   return (
